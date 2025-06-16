fca2d35b6d0182781705e239cab23daec3bc24d2
MAHESWAR REDDY <iammaheshreddy.ios26@gmail.com>
2025-06-16 17:58:11 +0530

    Added Booking overview and Modified design in Home page and Sort filter functionality

---
+++ HotelBooking/Screens/Dashboard/View/BookingOverviewVC.swift
@@ -0,0 +1,29 @@
+//
+//  BookingOverviewVC.swift
+//  HotelBooking
+//
+//  Created by toqsoft on 13/06/25.
+//
+
+import UIKit
+
+class BookingOverviewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
